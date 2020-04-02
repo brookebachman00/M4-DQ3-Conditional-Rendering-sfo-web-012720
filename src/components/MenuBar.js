420e71f8388e8fc637afdb0b111a3df9c8f96974
brookebachman00 <yourpetlover4411@yahoo.com>
2020-04-02 11:14:49 -0700

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const MenuBar = (props) => {
-
-  /*
+const MenuBar = props => {
+	/*
 
   The 'a' tags below are the menu items. Think about the way a menu 
   should work. When you click a menu item, the button typically becomes
@@ -13,26 +12,29 @@ const MenuBar = (props) => {
 
   */
 
-  return (
-    <div className="ui four item menu">
-      <a className="item active" id="profile">
-        <i className="user large icon" id="profile"/>
-      </a>
 
-      <a className="item" id="photo">
-        <i className="photo large icon" id="photo"/>
-      </a>
 
-      <a className="item" id="cocktail">
-        <i className="cocktail large icon" id="cocktail"/>
-      </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
-      </a>
-    </div>
-  )
 
-}
+	return (
+		<div onClick={props.changePage} className="ui four item menu">
+			<a className="item active" id="profile">
+				<i className="user large icon" id="profile" />
+			</a>
 
-export default MenuBar
+			<a className="item" id="photo">
+				<i className="photo large icon" id="photo" />
+			</a>
+
+			<a className="item" id="cocktail">
+				<i className="cocktail large icon" id="cocktail" />
+			</a>
+
+			<a className="item" id="pokemon">
+				<i className=" themeisle large icon" id="pokemon" />
+			</a>
+		</div>
+	);
+};
+
+export default MenuBar;
